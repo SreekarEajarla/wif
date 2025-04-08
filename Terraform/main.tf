28cb0fa69525761f1fe9316555a9afbcb6943786
Sreekar <Sreekar.Eajarla@bilvantis.io>
2025-04-08 11:16:53 +0530

    Added uniform bucket level access

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,6 +1,7 @@
 resource "google_storage_bucket" "buckets" {
-  count        = 5
-  name         = "my-bucket-${count.index + 1}-${var.project_id}"
-  location     = "US"
-  force_destroy = true
+  count                       = 5
+  name                        = "my-bucket-${count.index + 1}-${var.project_id}"
+  location                    = "US"
+  force_destroy               = true
+  uniform_bucket_level_access = true
 }
